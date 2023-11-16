872e0515e854f7007cd3ea26561b588d53a4a4ac
Beatriz Souza <beatriz.souza@ccc.ufcg.edu.br>
2023-11-16 17:42:44 +0100

    Move analysis of undefined elements

--- src/l3/Util.py
+++ src/l3/Util.py
@@ -1,3 +1,5 @@
+import libcst as cst
+
 def gather_files(files_arg, suffix=".py"):
     if all([f.endswith(".txt") for f in files_arg]):
         files = []
@@ -11,3 +13,21 @@ def gather_files(files_arg, suffix=".py"):
                 raise Exception(f"Incorrect argument, expected {suffix} file: {f}")
         files = files_arg
     return files
+
+def get_undefined_elements(file_path):
+    with open(file_path, "r") as file:
+        src = file.read()
+
+    undefined_elements = set()
+
+    ast = cst.parse_module(src)
+    ast_wrapper = cst.metadata.MetadataWrapper(ast)
+    scopes = set(ast_wrapper.resolve(cst.metadata.ScopeProvider).values())
+    ranges = ast_wrapper.resolve(cst.metadata.PositionProvider)
+    for scope in scopes:
+        for access in scope.accesses:
+            if len(access.referents) == 0:
+                node = access.node
+                undefined_elements.add(node.value)
+    
+    return undefined_elements
