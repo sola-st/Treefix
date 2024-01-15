947f327d7316205e898738de00eb9a5d115dfd75
Beatriz Souza <beatriz.souza@ccc.ufcg.edu.br>
2024-01-15 15:03:27 +0100

    Remove unused dependencies in setup.py

--- setup.py
+++ setup.py
@@ -23,10 +23,4 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
-    install_requires=[
-        'libcst',
-        'pandas',
-        'torch',
-        'tables',
-    ],
 )
