1345917e25bc75e6318d1612b063ddf181f097f8
Beatriz Souza <beatriz.souza@ccc.ufcg.edu.br>
2024-02-29 18:44:59 +0100

    Update workflow with GPT: keep conversation history, use different roles, sample answers, fix pos-processing and log format

--- src/l3/LLMs/GPT.py
+++ src/l3/LLMs/GPT.py
@@ -1,101 +1,121 @@
+import csv
 import os
-import re
 import json
 import time
 import openai
-import libcst as cst
+import pandas as pd
 from ..Util import get_json_info, install_dependencies, remove_lines_with_execution_error
 
 
 class GPTValuePredictor:
-    def __init__(self, openai_api_key, model_id="gpt-3.5-turbo"):
+    def __init__(self, openai_api_key, model_id="gpt-3.5-turbo-0125"):
         openai.api_key = openai_api_key
         self.model_id = model_id
-        
-    def predict(self, code_snippet_file, undefined_variables):
+        self.conversation_history = [
+            {
+                "role": "system", 
+                "content": "You are an exceptionally useful value predictor that consistently predicts accurate and reliable responses to user instructions."
+            }
+        ]
+    
+    def predict(self, prompt, code_snippet_file):
         self.code_snippet_file = code_snippet_file
-        prompt = self.create_prompt(code_snippet_file, undefined_variables)
-        print(prompt)
-        raw_predictions = self.query_model(prompt)
-        print(raw_predictions)
+        self.conversation_history.append({
+            "role": "user",
+            "content": prompt
+        })
+        raw_predictions = self.query_model()
         predictions = self.post_process_predictions(raw_predictions)
-        log_file = code_snippet_file.replace('.py', f'_{self.model_id}.json')
+        log_file = code_snippet_file.replace('.py', f'_{self.model_id}.csv')
         self.save_log(log_file, prompt, raw_predictions, predictions)
         return predictions
 
-    def create_prompt(self, code_snippet_file, undefined_variables):  
-        undefined_variables = '\n'.join(undefined_variables)
-
-        with open(code_snippet_file, "r") as file:
-            src = ''.join(file.readlines()[1:])
-
-        prompt = (
-            'Provide self-contained and concrete Python values to initialize the undefined variables in the code snippet.\n\n'
-            '# begin code snippet\n' 
-            f'{src}\n'
-            '# end code snippet\n\n'
-            '# begin undefined variables\n'
-            f'{undefined_variables}\n'
-            '# end undefined variables\n\n'
-            'Respond strictly with JSON. The JSON should be compatible with the TypeScript type `Response` from the following:\n\n'
-            '```ts\n'
-            'interface Response {\n'
-            '// Python import statements needed in the initialization code. One string per import.\n'
-            'imports: string[];\n'
-            '// Python code to initialize the undefined variables. One string per undefined variable. The statements in the code snippet above are not part of the initialization of undefined variables. Therefore, their copy or modified versions should not be added here. The values should be self-contained and concrete, i.e. without assumptions or expected replacements.\n'
-            'initialization: string[];\n'
-            '}\n'
-            '```'
-        )
-        
-        return prompt
-        
-    def query_model(self, prompt):
+    def query_model(self):
         while True:
             try:
                 response = openai.ChatCompletion.create(
                     model=self.model_id,
-                    messages=[{'role': 'user', 'content': prompt}],
+                    messages=self.conversation_history,
+                    response_format={ "type": "json_object" },
+                    n=10
                 )
                 break
             except Exception as e:
                 print(e)
                 # Rate limit achieved
                 time.sleep(60)
-        return response.choices[0].message.content
+        return response.choices
     
     def post_process_predictions(self, raw_predictions):
-        predictions = get_json_info(raw_predictions)
-        imports = '\n'.join(
-            [f'{import_} # pragma: no cover' for import_ in predictions['imports']]
-        )
-        initialization = '\n'.join(
-            [f'{init} # pragma: no cover' for init in predictions['initialization']]
-        )
+        post_processed_predictions = []
+        for raw_prediction in raw_predictions:
+            # Add predictions to conversation history
+            self.conversation_history.append({
+                "role": raw_prediction.message.role,
+                "content": raw_prediction.message.content
+            })
 
-        # Install missing dependencies
-        dependencies_dir_path = os.path.dirname(os.path.realpath(self.code_snippet_file))
-        install_dependencies(dependencies_dir_path, f'{imports}\n{initialization}')
+            prediction = get_json_info(raw_prediction)
 
-        # TO DO: add imports from the original snippet?
-        imports = remove_lines_with_execution_error(imports)
-        initialization_with_imports = '\n# AUX COMMENT: END IMPORTS AND BEGIN INITIALIZATION\n'.join(
-            [imports, initialization]
-        )
-        initialization_with_imports = remove_lines_with_execution_error(initialization_with_imports)
-        if imports:
-            initialization = initialization_with_imports.split('\n# AUX COMMENT: END IMPORTS AND BEGIN INITIALIZATION\n')[-1] 
-        else:
-             initialization = initialization_with_imports.replace('\n# AUX COMMENT: END IMPORTS AND BEGIN INITIALIZATION\n', '')  
+            # Add comment to avoid counting predictions during line coverage calculation
+            try:
+                imports = prediction['imports']
+                imports = [import_.replace("\n", "# pragma: no cover\n") for import_ in imports]
+                imports = '\n'.join(
+                    [f'{import_} # pragma: no cover' for import_ in imports]
+                )
+            except:
+                imports = ""
 
-        return {
-            'imports': imports,
-            'initialization': initialization
-        }
+            try:
+                initializations = prediction['initialization']
+                initializations = [initialization.replace("\n", "# pragma: no cover\n") for initialization in initializations]
+                initialization = '\n'.join(
+                    [f'{initialization} # pragma: no cover' for initialization in initializations]
+                )
+            except:
+                initialization = ""
+
+            # Install missing dependencies
+            dependencies_dir_path = os.path.dirname(os.path.realpath(self.code_snippet_file))
+            install_dependencies(dependencies_dir_path, f'{imports}\n{initialization}')
+
+            # Remove lines with execution errors
+            imports = remove_lines_with_execution_error(imports)
+            initialization_with_imports = '\n# AUX COMMENT: END IMPORTS AND BEGIN INITIALIZATION\n'.join(
+                [imports, initialization]
+            )
+            initialization_with_imports = remove_lines_with_execution_error(initialization_with_imports)
+            if imports:
+                initialization = initialization_with_imports.split('\n# AUX COMMENT: END IMPORTS AND BEGIN INITIALIZATION\n')[-1] 
+            else:
+                initialization = initialization_with_imports.replace('\n# AUX COMMENT: END IMPORTS AND BEGIN INITIALIZATION\n', '')  
+
+            post_processed_predictions.append({
+                'imports': imports,
+                'initialization': initialization
+            })
+
+        return post_processed_predictions
     
     def save_log(self, log_file, prompt, raw_predictions, predictions):
-        with open(log_file, 'w') as fp:
-            json.dump(dict(
-                prompt=prompt, 
-                raw_predictions=raw_predictions,
-                predictions=predictions), fp, indent=4)
+        predictions = {
+            'imports': predictions['imports'].split('\n'),
+            'initialization': predictions['initialization'].split('\n')
+        }
+        # Create CSV file and add header if it doesn't exist
+        if not os.path.isfile(log_file):
+            columns = ['prompt', 'raw_predictions', 'predictions']
+
+            with open(log_file, 'a') as csvFile:
+                writer = csv.writer(csvFile)
+                writer.writerow(columns)
+
+        df = pd.read_csv(log_file)
+        df_new_data = pd.DataFrame({
+            'prompt': [prompt],
+            'raw_predictions': [raw_predictions],
+            'predictions': [json.dumps(predictions, indent = 4)]
+        })
+        df = pd.concat([df, df_new_data])
+        df.to_csv(log_file, index=False)
--- src/l3/Run.py
+++ src/l3/Run.py
@@ -1,8 +1,10 @@
-import os
-import signal
+import re
+import time
 import argparse
 import subprocess
-from .Util import code_executes, gather_files, get_undefined_variables
+from .LLMs.GPT import GPTValuePredictor
+from .Prompt import Prompt
+from .Util import code_executes, gather_files, get_undefined_variables, get_undefined_attributes_methods
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -15,34 +17,90 @@ if __name__ == "__main__":
 
     files = gather_files(args.files)
 
+    prompt = Prompt()
+
     for file in files:
         with open(file, "r") as f:
             code = ''.join(f.readlines())
-
-        undefined_variables = get_undefined_variables(code)
-
-        # Original code contains undefined variables
-        if undefined_variables:
-            from .LLMs.GPT import GPTValuePredictor
-
-            # Predict missing values
-            predictor = GPTValuePredictor(args.openai_api_key)
-            predictions = predictor.predict(file, undefined_variables)
-            
-            imports = predictions['imports']
-            imports = imports + '\n\n' if imports else imports
-            updated_code = f'{imports}{code}'
-
-            # Code with predicted imports only is not executable
-            if not code_executes(updated_code):
-                initialization = predictions['initialization']
-                initialization = initialization + '\n\n' if initialization else initialization
-                updated_code = f'{imports}{initialization}{code}'
-        else:
             updated_code = code
+
+        predictor = GPTValuePredictor(args.openai_api_key)
+        successful_execution = code_executes(updated_code)
+        refine_attempts = 0
+
+        start_time = time.time()
+        
+        undefined_variables = get_undefined_variables(code)
+        undefined_attributes_methods = get_undefined_attributes_methods(code)
+
+        # TO DO: discard duplicate values from imports and initialization
+
+        if undefined_variables or undefined_attributes_methods:
+            # Predict undefined variables and attributes
+            var_prompt = prompt.generate_var(code, undefined_variables, undefined_attributes_methods)
+
+            predictions = predictor.predict(var_prompt, file)
+
+            for prediction in predictions:
+                imports = prediction['imports']
+                imports = imports + '\n\n' if imports else imports
+
+                updated_code = f'{imports}{code}'
+
+                # Code with predicted imports only is not executable
+                if not code_executes(updated_code):
+                    initialization = predictions['initialization']
+                    initialization = initialization + '\n\n' if initialization else initialization
+
+                    updated_code = f'{imports}{initialization}{code}'
+
+                    # Refine predictions
+                    while not successful_execution and refine_attempts < 10:
+                        with open("temp.py", "w") as f:
+                            f.write(updated_code)
+                        try:
+                            process = subprocess.run(["python3", "temp.py"], capture_output=True, text=True, check=True, timeout=30)
+                            successful_execution = True
+                        except subprocess.CalledProcessError as e:
+                            lines = e.stderr.splitlines()
+                            for line in lines:
+                                match = re.search(r'temp.py\", line (\d+)', line)
+                                if match:
+                                    line_number = int(match.group(1))
+
+                            line_code_and_error_msg = '\n'.join(lines[-2:])
+                            cleaned_error_msg = f'Execution error at line {line_number}:\n{line_code_and_error_msg}'
+                        
+                            refine_prompt = prompt.refine(cleaned_error_msg)
+                            refined_predictions = predictor.predict(refine_prompt, file)
+
+                            for refined_prediction in refined_predictions:
+                                refined_imports = refined_prediction['imports']
+                                refined_imports = refined_imports + '\n\n' if refined_imports else refined_imports
+
+                                imports = imports + refined_imports
+
+                                updated_code = f'{imports}{code}'
+
+                                # Code with predicted imports only is not executable
+                                if not code_executes(updated_code):
+                                    refined_initialization = refined_prediction['initialization']
+                                    refined_initialization = refined_initialization + '\n\n' if refined_initialization else refined_initialization
+
+                                    initialization = initialization + refined_initialization
+
+                                    updated_code = f'{imports}{initialization}{code}'
+                                else:
+                                    successful_execution = True
+                                    break
+                        except subprocess.TimeoutExpired:
+                            break
+
+                        refine_attempts += 1
+                        subprocess.run(["rm", "temp.py"])
 
         updated_file_path = file.replace('.py', '_updated.py')
         with open(updated_file_path, "w") as f:
             f.write(updated_code)
 
-        
+
